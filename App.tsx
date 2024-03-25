4d0b58fd1854d636d404e0c7ad42a29966ad40b3
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-25 20:04:08 -0300

    chore: organize imports

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { RootNavigation } from "@routes/stack-native";
 
 import "./global.css";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
     return (
