7b5cd13f923dd8f9cd9e5aae38fe05a088c4b12e
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-01 18:09:03 -0300

    feat: refactor and fix styling bugs in loading component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { TrendingMovies } from "./trending-movies/trending-movies";
 export { MoviesList } from "./movies-list/movies-list";
 export { Cast } from "./cast/cast";
-export { Loading } from "./loading/loading";
--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import { Dimensions, View } from "react-native";
+import { View } from "react-native";
 import * as Progress from "react-native-progress";
 
 import { theme } from "@themes/index";
 
-const { width, height } = Dimensions.get("window");
-
 type LoadingProps = {
     testID?: string;
+    size?: number;
 };
 
-export function Loading({ testID = "components.loading" }: LoadingProps) {
+export function Loading({
+    testID = "components.loading",
+    size = 160,
+}: LoadingProps) {
     return (
         <View
             testID={testID}
-            style={{ height, width }}
-            className='absolute flex-row justify-center items-center'>
+            className='flex-row justify-center items-center mt-10'>
             <Progress.CircleSnail
                 thickness={12}
-                size={160}
+                size={size}
                 color={theme.background}
                 testID='components.loading.circle-snail'
             />
