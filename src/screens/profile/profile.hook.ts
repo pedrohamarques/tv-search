00dd54adc69ed79805acba38cb3ab9e26e29d89a
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-09 15:13:53 -0300

    feat: add initial functionalities in profile screen

--- src/screens/profile/profile.hook.ts
+++ src/screens/profile/profile.hook.ts
@@ -1,3 +1,69 @@
+import { useState } from "react";
+import { z } from "zod";
+import { useNavigation } from "@react-navigation/native";
+import * as ImagePicker from "expo-image-picker";
+import Toast from "react-native-toast-message";
+
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import type { RootStackParamsList } from "@typings/route";
+
 export function useProfileScreen() {
-    return {};
+    const [avatar, setAvatar] = useState<string | null>(null);
+    const [name, setName] = useState("");
+    const [isEditing, setIsEditing] = useState(false);
+
+    const navigation =
+        useNavigation<NativeStackNavigationProp<RootStackParamsList>>();
+
+    function handleBackPress() {
+        navigation.goBack();
+    }
+
+    const nameSchema = z.string().trim().min(3);
+
+    async function handlePickImage() {
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 0.7,
+            });
+
+            if (!result.canceled) {
+                setAvatar(result.assets[0].uri);
+            }
+        } catch (e) {
+            console.log(e);
+            Toast.show({
+                type: "error",
+                text1: "Couldn't pick image",
+                text2: "Try again later",
+                onPress: () => Toast.hide(),
+            });
+        }
+    }
+
+    function handleEditingPress() {
+        if (isEditing) {
+            const confirmPress = nameSchema.safeParse(name);
+            if (!confirmPress.success) {
+                Toast.show({
+                    type: "error",
+                    text1: "Please, try other name",
+                    text2: "Name must be at least 3 characters long",
+                    onPress: () => Toast.hide(),
+                });
+            }
+        }
+        setIsEditing(previousState => !previousState);
+    }
+    return {
+        handleBackPress,
+        handlePickImage,
+        avatar,
+        isEditing,
+        handleEditingPress,
+        setName,
+    };
 }
--- src/screens/profile/profile.tsx
+++ src/screens/profile/profile.tsx
@@ -1,10 +1,136 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import {
+    Text,
+    TouchableOpacity,
+    View,
+    KeyboardAvoidingView,
+    ScrollView,
+    Platform,
+} from "react-native";
+import { ChevronLeftIcon } from "react-native-heroicons/solid";
+import { FontAwesome6 } from "@expo/vector-icons";
 
-export function ProfileScreen() {
+import { styles } from "@themes/index";
+
+import { useProfileScreen } from "./profile.hook";
+
+import { RootStackParamsList } from "@typings/route";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Avatar } from "@components/avatar";
+import { Input } from "@components/input";
+import { PencilIcon, CheckIcon } from "react-native-heroicons/outline";
+
+type ProfileScreenNavigationProps = {
+    navigation: Pick<
+        NativeStackNavigationProp<RootStackParamsList>,
+        "setOptions"
+    >;
+};
+
+export function ProfileScreen({ navigation }: ProfileScreenNavigationProps) {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <TouchableOpacity
+                    style={[
+                        styles.background,
+                        { padding: 1, borderRadius: 10 },
+                    ]}
+                    onPress={handleBackPress}>
+                    <ChevronLeftIcon
+                        size='24'
+                        strokeWidth={2.5}
+                        color='white'
+                    />
+                </TouchableOpacity>
+            ),
+        });
+    }, [navigation]);
+
+    const {
+        handleBackPress,
+        handlePickImage,
+        avatar,
+        isEditing,
+        handleEditingPress,
+        setName,
+    } = useProfileScreen();
     return (
-        <View>
-            <Text>Profile Screen</Text>
-        </View>
+        <KeyboardAvoidingView
+            className='flex-1 bg-neutral-800 justify-start item-center'
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={150}>
+            <ScrollView className='px-2 overflow-visible'>
+                <View className='w-full px-4 mt-6 mb-8'>
+                    <Text className='text-white text-3xl font-bold'>
+                        Profile
+                    </Text>
+                </View>
+                <View className=' justify-center items-center mb-4'>
+                    <Avatar onAvatarPress={handlePickImage} image={avatar} />
+                </View>
+
+                <View className='mt-4 px-4'>
+                    <Input title='Name' isEditing={isEditing}>
+                        <FontAwesome6
+                            name='user-circle'
+                            size={20}
+                            color={styles.background.backgroundColor}
+                        />
+                        <Input.Field
+                            placeholder='Insert your name'
+                            keyboardType='default'
+                            autoCorrect={false}
+                            editable={isEditing}
+                            onChangeText={setName}
+                            isEditing={isEditing}
+                        />
+                        <TouchableOpacity
+                            onPress={handleEditingPress}
+                            style={{ paddingRight: 4 }}>
+                            {!isEditing ? (
+                                <PencilIcon
+                                    size={20}
+                                    strokeWidth={2.5}
+                                    color={styles.background.backgroundColor}
+                                />
+                            ) : (
+                                <CheckIcon
+                                    size={20}
+                                    stroke={styles.background.backgroundColor}
+                                    strokeWidth={2.5}
+                                    color={styles.background.backgroundColor}
+                                />
+                            )}
+                        </TouchableOpacity>
+                    </Input>
+                    <Input title='E-mail'>
+                        <FontAwesome6
+                            name='envelope-open'
+                            size={20}
+                            color={styles.background.backgroundColor}
+                        />
+                        <Input.Field
+                            placeholder='Insert your e-mail'
+                            keyboardType='email-address'
+                            textContentType='emailAddress'
+                            autoCorrect={false}
+                            editable={false}
+                        />
+                    </Input>
+                    <Input title='Country'>
+                        <FontAwesome6
+                            name='flag'
+                            size={20}
+                            color={styles.background.backgroundColor}
+                        />
+                        <Input.Field
+                            placeholder='Insert your country'
+                            textContentType='countryName'
+                        />
+                    </Input>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
