6cd025a353673b55170c752335cc8efaaf261d54
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-23 15:41:49 -0300

    feat: add error and loading state to cast component

--- src/components/cast/__tests__/cast.test.tsx
+++ src/components/cast/__tests__/cast.test.tsx
@@ -7,6 +7,8 @@ import { DUMMY_CAST } from "./dummy";
 const mockValues = {
     cast: DUMMY_CAST,
     handleCastPress: () => {},
+    isLoading: false,
+    hasError: false,
 };
 
 describe("components/cast/<Cast />", () => {
@@ -20,5 +22,45 @@ describe("components/cast/<Cast />", () => {
         expect(screen.getByText("Top Cast")).toBeTruthy();
         expect(screen.getByTestId("components.cast.cast-item-0")).toBeTruthy();
         expect(screen.getByTestId("components.cast.cast-item-1")).toBeTruthy();
+
+        expect(screen.queryByTestId("components.cast.loading")).toBeNull();
+        expect(
+            screen.queryByText(
+                "There was some error when fetching data. Please try again.",
+            ),
+        ).toBeNull();
+    });
+
+    it("renders component properly when it is loading", () => {
+        render(<Cast {...mockValues} isLoading />);
+
+        expect(screen.getByText("Top Cast")).toBeTruthy();
+
+        expect(screen.queryByTestId("components.cast.cast-item-0")).toBeNull();
+        expect(screen.queryByTestId("components.cast.cast-item-1")).toBeNull();
+
+        expect(screen.getByTestId("components.cast.loading")).toBeTruthy();
+
+        expect(
+            screen.queryByText(
+                "There was some error when fetching data. Please try again.",
+            ),
+        ).toBeNull();
+    });
+
+    it("renders component properly when it has some error", () => {
+        render(<Cast {...mockValues} hasError />);
+
+        expect(screen.getByText("Top Cast")).toBeTruthy();
+
+        expect(screen.queryByTestId("components.cast.cast-item-0")).toBeNull();
+        expect(screen.queryByTestId("components.cast.cast-item-1")).toBeNull();
+        expect(screen.queryByTestId("components.cast.loading")).toBeNull();
+
+        expect(
+            screen.getByText(
+                "There was some error when fetching data. Please try again.",
+            ),
+        ).toBeTruthy();
     });
 });
--- src/components/cast/cast.tsx
+++ src/components/cast/cast.tsx
@@ -1,39 +1,59 @@
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
+
+import { Loading } from "@components/loading";
+
 import { CastItem } from "./components/cast-item";
 
-import { CastType } from "@typings/data";
+import type { CastType } from "@typings/data";
 
 type CastProps = {
     cast: CastType[];
     handleCastPress: (cast: CastType) => void;
     testID?: string;
+    isLoading?: boolean;
+    hasError: boolean;
 };
 
 export function Cast({
     cast,
     handleCastPress,
     testID = "components.cast",
+    isLoading,
+    hasError,
 }: CastProps) {
     return (
         <View className='my-6' testID={testID}>
             <Text className=' text-white text-lg mx-4 mb-5'>Top Cast</Text>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{ paddingHorizontal: 6 }}>
-                {cast &&
-                    cast.map((person, index) => (
-                        <CastItem
-                            key={index}
-                            personName={person.name}
-                            characterName={person.character}
-                            imagePath={person.profile_path}
-                            handlePress={() => handleCastPress(person)}
-                            testID={`components.cast.cast-item-${index}`}
-                        />
-                    ))}
-            </ScrollView>
+            {isLoading ? (
+                <View className={"-mt-10"}>
+                    <Loading size={80} testID='components.cast.loading' />
+                </View>
+            ) : hasError && !isLoading ? (
+                <View className='justify-center items-center my-6'>
+                    <Text className='text-white text-l align-center'>
+                        There was some error when fetching data. Please try
+                        again.
+                    </Text>
+                </View>
+            ) : (
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{ paddingHorizontal: 6 }}>
+                    {cast &&
+                        cast.map((person, index) => (
+                            <CastItem
+                                key={index}
+                                personName={person.name}
+                                characterName={person.character}
+                                imagePath={person.profile_path}
+                                handlePress={() => handleCastPress(person)}
+                                testID={`components.cast.cast-item-${index}`}
+                            />
+                        ))}
+                </ScrollView>
+            )}
         </View>
     );
 }
