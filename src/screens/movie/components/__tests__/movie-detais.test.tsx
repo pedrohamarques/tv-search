00a09dd11772c18fc199157d9077a78742edcae7
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-23 15:54:02 -0300

    feat: refactor movie details component to receive loading and error state

--- src/screens/movie/components/__tests__/movie-detais.test.tsx
+++ src/screens/movie/components/__tests__/movie-detais.test.tsx
@@ -4,13 +4,19 @@ import { render, screen } from "@testing-library/react-native";
 import { MovieDetails } from "../movie-details";
 import { DUMMY_MOVIE_DETAILS } from "@constants/data";
 
+const mockValues = {
+    movie: DUMMY_MOVIE_DETAILS,
+    isLoading: false,
+    hasError: false,
+};
+
 describe("screens/movie/components/movie-details/<MovieDetails />", () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
     it("renders component properly", () => {
-        render(<MovieDetails movie={DUMMY_MOVIE_DETAILS} />);
+        render(<MovieDetails {...mockValues} />);
 
         expect(
             screen.getByTestId("screens.movie.component.movie-details.image"),
@@ -21,6 +27,65 @@ describe("screens/movie/components/movie-details/<MovieDetails />", () => {
         expect(
             screen.getByText(
                 "The true story of anti-apartheid activists in South Africa, and particularly the life of Patrick Chamusso, a timid foreman at Secunda CTL, the largest synthetic fuel plant in the world. Patrick is wrongly accused, imprisoned and tortured for an attempt to bomb the plant, with the injustice transforming the apolitical worker into a radicalised insurgent, who then carries out his own successful sabotage mission.",
+            ),
+        ).toBeTruthy();
+
+        expect(
+            screen.queryByTestId("components.movie-details.loading"),
+        ).toBeNull();
+        expect(
+            screen.queryByText(
+                "There was some error when fetching data. Please try again.",
+            ),
+        ).toBeNull();
+    });
+
+    it("renders screen properly when it is loading", () => {
+        render(<MovieDetails {...mockValues} isLoading />);
+
+        expect(
+            screen.queryByTestId("screens.movie.component.movie-details.image"),
+        ).toBeNull();
+        expect(screen.queryByText("Catch a Fire")).toBeNull();
+        expect(screen.queryByText("Released • 2006 • 101 min")).toBeNull();
+        expect(screen.queryByText("Action")).toBeNull();
+        expect(
+            screen.queryByText(
+                "The true story of anti-apartheid activists in South Africa, and particularly the life of Patrick Chamusso, a timid foreman at Secunda CTL, the largest synthetic fuel plant in the world. Patrick is wrongly accused, imprisoned and tortured for an attempt to bomb the plant, with the injustice transforming the apolitical worker into a radicalised insurgent, who then carries out his own successful sabotage mission.",
+            ),
+        ).toBeNull();
+
+        expect(
+            screen.getByTestId("components.movie-details.loading"),
+        ).toBeTruthy();
+        expect(
+            screen.queryByText(
+                "There was some error when fetching data. Please try again.",
+            ),
+        ).toBeNull();
+    });
+
+    it("renders component properly when it has error", () => {
+        render(<MovieDetails {...mockValues} hasError />);
+
+        expect(
+            screen.queryByTestId("screens.movie.component.movie-details.image"),
+        ).toBeNull();
+        expect(screen.queryByText("Catch a Fire")).toBeNull();
+        expect(screen.queryByText("Released • 2006 • 101 min")).toBeNull();
+        expect(screen.queryByText("Action")).toBeNull();
+        expect(
+            screen.queryByText(
+                "The true story of anti-apartheid activists in South Africa, and particularly the life of Patrick Chamusso, a timid foreman at Secunda CTL, the largest synthetic fuel plant in the world. Patrick is wrongly accused, imprisoned and tortured for an attempt to bomb the plant, with the injustice transforming the apolitical worker into a radicalised insurgent, who then carries out his own successful sabotage mission.",
+            ),
+        ).toBeNull();
+
+        expect(
+            screen.queryByTestId("components.movie-details.loading"),
+        ).toBeNull();
+        expect(
+            screen.getByText(
+                "There was some error when fetching data. Please try again.",
             ),
         ).toBeTruthy();
     });
--- src/screens/movie/components/movie-details.tsx
+++ src/screens/movie/components/movie-details.tsx
@@ -4,57 +4,80 @@ import { Dimensions, Image, Text, View } from "react-native";
 import { imageURI } from "@constants/url";
 
 import { MovieDetailsResponse } from "@typings/data";
+import { Loading } from "@components/loading";
 
 const { width, height } = Dimensions.get("window");
 
 type MovieDetailsProps = {
     movie: MovieDetailsResponse;
     testID?: string;
+    hasError: boolean;
+    isLoading?: boolean;
 };
 
 export function MovieDetails({
     movie,
     testID = "screens.movie.components.movie-details",
+    hasError,
+    isLoading,
 }: MovieDetailsProps) {
     return (
         <>
-            <View testID={testID}>
-                <Image
-                    source={{
-                        uri: `${imageURI}${movie.poster_path}`,
-                    }}
-                    style={{
-                        width: width * 0.85,
-                        height: height * 0.55,
-                    }}
-                    className='rounded-2xl self-center mt-4'
-                    testID='screens.movie.component.movie-details.image'
-                />
-            </View>
-            <View className='space-y-3 my-10'>
-                <Text className='text-white text-center text-3xl font-bold tracking-wider'>
-                    {movie.title}
-                </Text>
-                <Text className='text-neutral-400 font-semibold text-base text-center mt-2'>
-                    {movie.status} • {movie.release_date.split("-")[0]} •{" "}
-                    {movie.runtime} min
-                </Text>
-
-                <View className='flex-row justify-center mx-4 space-x-2 my-4'>
-                    {movie.genres.map((item, index) => (
-                        <Text
-                            key={index}
-                            className='text-neutral-400 font-semibold text-base text-center'>
-                            {item.name}{" "}
-                            {index !== movie.genres.length - 1 && "• "}
-                        </Text>
-                    ))}
+            {isLoading ? (
+                <View className='mb-10'>
+                    <Loading
+                        size={140}
+                        testID='components.movie-details.loading'
+                    />
                 </View>
+            ) : hasError && !isLoading ? (
+                <View className='justify-center items-center my-10'>
+                    <Text className='text-white text-l align-center'>
+                        There was some error when fetching data. Please try
+                        again.
+                    </Text>
+                </View>
+            ) : (
+                <>
+                    <View testID={testID}>
+                        <Image
+                            source={{
+                                uri: `${imageURI}${movie.poster_path}`,
+                            }}
+                            style={{
+                                width: width * 0.85,
+                                height: height * 0.55,
+                            }}
+                            className='rounded-2xl self-center mt-4'
+                            testID='screens.movie.component.movie-details.image'
+                        />
+                    </View>
+                    <View className='space-y-3 my-10'>
+                        <Text className='text-white text-center text-3xl font-bold tracking-wider'>
+                            {movie.title}
+                        </Text>
+                        <Text className='text-neutral-400 font-semibold text-base text-center mt-2'>
+                            {movie.status} • {movie.release_date.split("-")[0]}{" "}
+                            • {movie.runtime} min
+                        </Text>
 
-                <Text className='text-neutral-400 mx-4 tracking-wide mt-4'>
-                    {movie.overview}
-                </Text>
-            </View>
+                        <View className='flex-row justify-center mx-4 space-x-2 my-4'>
+                            {movie.genres.map((item, index) => (
+                                <Text
+                                    key={index}
+                                    className='text-neutral-400 font-semibold text-base text-center'>
+                                    {item.name}{" "}
+                                    {index !== movie.genres.length - 1 && "• "}
+                                </Text>
+                            ))}
+                        </View>
+
+                        <Text className='text-neutral-400 mx-4 tracking-wide mt-4'>
+                            {movie.overview}
+                        </Text>
+                    </View>
+                </>
+            )}
         </>
     );
 }
