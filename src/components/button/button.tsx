e8e59777807261259736a65ff02394f09e2e5fd9
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-25 11:38:28 -0300

    fix: refactor button to add testID to logo

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -21,11 +21,11 @@ export function Button({
             <View className='bg-white flex-row justify-between items-center p-4 rounded-md shadow-sm gap-4'>
                 <Text className='font-semibold text-md'>{title}</Text>
                 {Logo && (
-                    <Logo
-                        width={24}
-                        height={24}
-                        testID='components.button.logo'
-                    />
+                    <View
+                        className='h-6 w-6 justify-center items-center'
+                        testID='components.button.logo'>
+                        <Logo width={24} height={24} />
+                    </View>
                 )}
             </View>
         </TouchableOpacity>
