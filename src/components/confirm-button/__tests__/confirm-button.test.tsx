25f0685b799921337de0651d6a5b8706786265d7
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-27 16:08:22 -0300

    fix: fix confirm-button component tests

--- src/components/confirm-button/__tests__/confirm-button.test.tsx
+++ src/components/confirm-button/__tests__/confirm-button.test.tsx
@@ -3,13 +3,17 @@ import { render, screen } from "@testing-library/react-native";
 
 import { ConfirmButton } from "../confirm-button";
 
+const mockValues = {
+    title: "Update Profile",
+};
+
 describe("components/confirm-button/<ConfirmButton />", () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
     it("renders component properly", () => {
-        render(<ConfirmButton />);
+        render(<ConfirmButton {...mockValues} />);
 
         expect(screen.getByTestId("components.confirm-button")).toBeTruthy();
         expect(screen.getByText("Update Profile")).toBeTruthy();
